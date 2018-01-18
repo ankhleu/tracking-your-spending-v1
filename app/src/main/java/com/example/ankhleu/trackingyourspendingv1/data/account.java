5f45a8b905fd404f3ed40db355020760d2cac324
Ankhleu <summerlightwind@gmail.com>
2018-01-19 01:16:35 +0800

    設定待討論，預先設定變數

--- app/src/main/java/com/example/ankhleu/trackingyourspendingv1/data/account.java
+++ app/src/main/java/com/example/ankhleu/trackingyourspendingv1/data/account.java
@@ -7,5 +7,18 @@ package com.example.ankhleu.trackingyourspendingv1.data;
 public class account {
 
 
+String year;    //年
+String month;   //月
+String day;     //日
+int Budget;     //預算
+int pay;        //付錢金額
+String kind;    //類別
+String myself;  //沒活動類別
+String travel;  //有活動類別
+String receipt; //收據
+String notes;   //備註
+
+
+
 
 }
