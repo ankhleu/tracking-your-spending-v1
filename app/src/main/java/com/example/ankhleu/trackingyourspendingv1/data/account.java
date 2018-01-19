8cf52327631924bfc7a50ab11f7aa0f7268ef37d
Ankhleu <summerlightwind@gmail.com>
2018-01-19 15:05:54 +0800

    修改pay變數為Arraylist<string>類別,活動刪除預算textview

--- app/src/main/java/com/example/ankhleu/trackingyourspendingv1/data/account.java
+++ app/src/main/java/com/example/ankhleu/trackingyourspendingv1/data/account.java
@@ -1,5 +1,9 @@
 package com.example.ankhleu.trackingyourspendingv1.data;
 
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
 /**
  * Created by ankhleu on 2018/1/18.
  */
@@ -11,7 +15,10 @@ String year;    //年
 String month;   //月
 String day;     //日
 int Budget;     //預算
-int pay;        //付錢金額
+
+ArrayList<ArrayList<String>> pay=new ArrayList<ArrayList<String>>();        //pay為付錢金額
+
+
 String kind;    //類別
 String myself;  //沒活動類別
 String travel;  //有活動類別
